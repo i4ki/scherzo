6cdba3a7384927c807d03a772be6bd65caffdec5
Tiago Natel de Moura <tiago4orion@gmail.com>
2015-06-11 11:26:23 -0300

    added example to main

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/tiago4orion/scherzo/lang"
+)
 
 const version = "0.0.1"
 
 func main() {
 	fmt.Println("Scherzo compiler ", version)
+
+	value2 := lang.Cons(func(uint) interface{} {
+		return 2
+	}, func(uint) interface{} {
+		return nil
+	})
+
+	values := lang.Cons(func(uint) interface{} {
+		return 1
+	}, func(uint) interface{} {
+		return value2
+	})
+
+	// values == '(1 . (2 . ()))
+	ret := lang.Apply(lang.Plus, values)
+
+	fmt.Println(ret(1))
 }
